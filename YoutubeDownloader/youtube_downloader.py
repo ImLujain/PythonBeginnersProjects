89edd65d53874e3be6346c8facdd742ae98d020c
imLujain <lujaindev@gmail.com>
2022-04-19 12:23:00 +0300

    .

--- YoutubeDownloader/youtube_downloader.py
+++ YoutubeDownloader/youtube_downloader.py
@@ -13,12 +13,16 @@ def file_format(video_url, video_format):
     video_info = youtube_dl.YoutubeDL().extract_info(
     url = video_url,download=False)
 
-    #print(video_info)
-
-    filename = f"{video_info['title']}.mp3"
-    print(filename)
+    filename = f"{video_info['title']}."+video_format
+    if video_format == "mp3":
+        format = "bestaudio"
+    elif video_format == "mp4":
+        format = "bestvideo"
+    else:
+        print(u"\u001b[31m *** Format must be mp3 or mp4")
+        exit(1)
     options={
-        'format':'bestaudio/best',
+        'format':format+'/best',
         'keepvideo':False,
         'outtmpl':filename,
     }
@@ -32,17 +36,17 @@ if __name__=='__main__':
     video_url = None
     video_format = None
     argv = sys.argv[1:]
-    # try:
-    opts, args = getopt.getopt(argv, "l:f:", ['link=',  'format='] )
-    for opt, arg in opts:
+    try:
+        opts, args = getopt.getopt(argv, "l:f:", ['link=',  'format='] )
+        for opt, arg in opts:
             if opt in ['-l', '--link']:
                 video_url = arg
             elif opt in ['-f', '--format']:
                 video_format = arg
-    file_format(video_url, video_format)
-    # except Exception as e:
-    #     print("Usage: Pyhton youtube_downloader.py --url --format")
-    #     print(e)
+        file_format(video_url, video_format)
+    except Exception as e:
+         print("Usage: Pyhton youtube_downloader.py --link= --format=")
+
 
 
 
